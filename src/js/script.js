c44707e60c60fc0c642061aa8336de61fe1780a8
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-16 17:09:53 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
+// let name = prompt('Введіть ваше ім’я:');
+// alert(`Привіт, ${name}!`)
+
+// const year = 2023;
+// let userYear = prompt('Введіть ваш рік народження:');
+// alert(`Ваш вік: ${year - userYear} років`)
+
+// let sideOfSquare = prompt('Придумайте та введіть сторону квадрата:');
+// alert(`Периметр квадрата=${sideOfSquare *4} см`)
+
+// let circleRadius = prompt('Придумайте та введіть радіус кола:');
+// alert(`Площа кола=${circleRadius*circleRadius*Math.PI}`)
 
