1012a05b3e7d93b1a0a430dc1b5530c3ccf28de5
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-17 10:48:37 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,38 @@
+/*Перше*/
+
 // let name = prompt('Введіть ваше ім’я:');
 // alert(`Привіт, ${name}!`)
+
+/*Друге*/
 
 // const year = 2023;
 // let userYear = prompt('Введіть ваш рік народження:');
 // alert(`Ваш вік: ${year - userYear} років`)
 
+/*Третє*/
+
 // let sideOfSquare = prompt('Придумайте та введіть сторону квадрата:');
 // alert(`Периметр квадрата=${sideOfSquare *4} см`)
 
+/*Четверте*/
+
 // let circleRadius = prompt('Придумайте та введіть радіус кола:');
 // alert(`Площа кола=${circleRadius*circleRadius*Math.PI}`)
+
+/*П’яте*/
+
+/*let distance = prompt('Введіть відстань у кілометрах між двома містами');
+let hour = prompt('Введіть кількість годин за скільки ви хочете добратися');
+alert(`Ваша швидкість, щоб встигнути вчасно = ${distance / hour} км/год`)*/
+
+/*Шосте*/
 
 /*const dollar = 40.2;
 const euro = 42.5;
 
 let valuta = prompt('Введіть суму в доларах');
 alert(`Ваша сумма в євро: ${ valuta * dollar / euro} євро`)*/
+
 
 
 
