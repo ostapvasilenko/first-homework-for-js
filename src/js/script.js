684a2f5f10764999e65149f1ddf2042d0133fec3
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-01-18 12:02:36 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,73 +1,73 @@
 /*Перше*/
 
-// let name = prompt('Введіть ваше ім’я:');
-// alert(`Привіт, ${name}!`)
+let name = prompt('Введіть ваше ім’я:');
+alert(`Привіт, ${name}!`)
 
 /*Друге*/
 
-// const year = 2023;
-// let userYear = prompt('Введіть ваш рік народження:');
-// alert(`Ваш вік: ${year - userYear} років`)
+const year = 2023;
+let userYear = prompt('Введіть ваш рік народження:');
+alert(`Ваш вік: ${year - userYear} років`)
 
 /*Третє*/
 
-// let sideOfSquare = prompt('Придумайте та введіть сторону квадрата:');
-// alert(`Периметр квадрата=${sideOfSquare * 4} см`)
+let sideOfSquare = prompt('Придумайте та введіть сторону квадрата:');
+alert(`Периметр квадрата=${sideOfSquare * 4} см`)
 
 /*Четверте*/
 
-// let circleRadius = prompt('Придумайте та введіть радіус кола:');
-// alert(`Площа кола=${circleRadius * circleRadius * Math.PI}`)
+let circleRadius = prompt('Придумайте та введіть радіус кола:');
+alert(`Площа кола=${circleRadius * circleRadius * Math.PI}`)
 
 /*П’яте*/
 
-/*let distance = prompt('Введіть відстань у кілометрах між двома містами');
+let distance = prompt('Введіть відстань у кілометрах між двома містами');
 let hour = prompt('Введіть кількість годин за скільки ви хочете добратися');
-alert(`Ваша швидкість, щоб встигнути вчасно = ${(distance / hour).toFixed(2)} км/год`)*/
+alert(`Ваша швидкість, щоб встигнути вчасно = ${(distance / hour).toFixed(2)} км/год`)
 
 /*Шосте*/
 
-/*const dollar = 40.2;
+const dollar = 40.2;
 const euro = 42.5;
 
 let valuta = prompt('Введіть суму в доларах');
-alert(`Ваша сумма в євро: ${(valuta * dollar / euro).toFixed(2)} євро`)*/
+alert(`Ваша сумма в євро: ${(valuta * dollar / euro).toFixed(2)} євро`)
 
 /*Сьоме*/
 
-// const size = 820;
-// const sizeNext = 1024;
-// let userSize = prompt('Введіть обсяг флешки в ГБ');
-// alert(`Кількість файлі розміром 820 МБ які вмістяться на флешці дорівнює: ${Math.floor(userSize * sizeNext / size)}`)
+const size = 820;
+const sizeNext = 1024;
+let userSize = prompt('Введіть обсяг флешки в ГБ');
+alert(`Кількість файлі розміром 820 МБ які вмістяться на флешці дорівнює: ${Math.floor(userSize * sizeNext / size)}`)
 
 /*Восьме*/
 
-/*let sumMoney = prompt('Введіть вашу суму грошей (грн)');
+let sumMoney = prompt('Введіть вашу суму грошей (грн) в гаманці');
 let costOfChocolade = prompt('Введіть ціну однієї шоколадки');
-let numChocolate =  Math.floor(sumMoney / costOfChocolade);
+let numChocolate = Math.floor(sumMoney / costOfChocolade);
 alert(`Кількість шоколадок, яку ви можете купити за ваші гроші: ${numChocolate} шт.  
-Ваша решта: ${sumMoney-(numChocolate*costOfChocolade)} грн`);
-*/
+Ваша решта: ${sumMoney - (numChocolate * costOfChocolade)} грн`);
+
 
 /*Дев’яте*/
 
-let number = prompt('Введіть будь яке тризначне число')
- a = '';
-for (let i = 1; i >= 0 ; i--) {
-   a += "" +number[i];
+let numbers = prompt('Введіть будь яке тризначне число')
+a = '';
+for (let i = 1; i >= 0; i--) {
+    a += "" + numbers[i];
 }
-alert(`З вашого числа вийшло таке число паліндром: ${number + "" +a}`)
+alert(`З вашого числа вийшло таке число паліндром: ${numbers + "" + a}`)
 
 
 /*Десяте*/
 
-/*let number = prompt('Введіть будь яке ціле число')
-if (number % 2 == 0){
-alert(`Число парне!`)
-} else if (number % 2 != 0){
+let number = prompt('Введіть будь яке ціле число')
+if (number % 2 == 0) {
+    alert(`Число парне!`)
+} else if (number % 2 != 0) {
     alert(`Число непарне!`)
 }
-*/
+
 
 
 
